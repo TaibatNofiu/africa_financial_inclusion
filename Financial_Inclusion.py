d3ccf0d81849dc319c491b99e3933c6a553537a4
TaibatNofiu <147071304+TaibatNofiu@users.noreply.github.com>
2025-04-25 05:29:54 +0100

    Update Financial_Inclusion.py

--- Financial_Inclusion.py
+++ Financial_Inclusion.py
@@ -15,7 +15,7 @@ st.sidebar.header("Input features for prediction")
 
 # Load the dataset
 #uploaded_file = st.file_uploader('Desktop/GMC/ML/Financial_inclusion_dataset.csv', type = 'csv')
-data = pd.read_csv(r'Desktop/GMC/ML/Financial_inclusion_dataset.csv')
+data = pd.read_csv(r'Desktop/GMC/Skillsets/financial_inclusion/Financial_inclusion_dataset.csv')
 # Chedk for outliers
 # Z-score method (for normally distributed data)
 from scipy import stats
@@ -38,24 +38,51 @@ cat_cols = ['country', 'location_type', 'cellphone_access',
 # Display the dataset on the deploy page
 st.write('Financial Inclusion Dataset', financial_data.sample(100))
 # Set the feature for the inputs
-country = st.sidebar.selectbox('Country', financial_data['country'].unique())
-location = st.sidebar.selectbox('Location', financial_data['location_type'].unique())
-cellphone = st.sidebar.selectbox('Cellphone', financial_data['cellphone_access'].unique())
-household_size = st.sidebar.slider('Household_Size',
+def user_inputs():
+    country = st.sidebar.selectbox('Country', financial_data['country'].unique())
+    location = st.sidebar.selectbox('Location', financial_data['location_type'].unique())
+    cellphone = st.sidebar.selectbox('Cellphone', financial_data['cellphone_access'].unique())
+    household_size = st.sidebar.slider('Household_Size',
                                 int(financial_data['household_size'].min()),
                                 int(financial_data['household_size'].mean()),
                                 int(financial_data['household_size'].max()))
-respondent_age = st.sidebar.slider('Respondent_Age',
+    respondent_age = st.sidebar.slider('Respondent_Age',
                                 int(financial_data['age_of_respondent'].min()),
                                 int(financial_data['age_of_respondent'].mean()),
                                 int(financial_data['age_of_respondent'].max()))
-gender = st.sidebar.selectbox('Gender', financial_data['gender_of_respondent'].unique())
-relationship = st.sidebar.selectbox('Relationship_With_Head', financial_data['relationship_with_head'])
+    gender = st.sidebar.selectbox('Gender', financial_data['gender_of_respondent'].unique())
+    relationship = st.sidebar.selectbox('Relationship_With_Head', financial_data['relationship_with_head'])
+    # Prepare a dataframe for users to input features
+    data_features = {'Country': [country],
+                          'Location': [location],
+                          'Cellphone': [cellphone],
+                          'Household_Size': [household_size],
+                          'Respondent_Age': [respondent_age],
+                          'Gender': [gender],
+                          'Relationship': [relationship]}
+    user_input = pd.DataFrame(data_features, index = [0])
+    return user_input
+inputs_df = user_inputs()
+
 # Encode categorical columns
 financial_encode = pd.get_dummies(financial_data, columns = cat_cols)
+# Balance the data set
+from sklearn.utils import resample
+# Separate the minority and majority
+minority = financial_encode[financial_encode['bank_account'] == 'Yes']
+majority = financial_encode[financial_encode['bank_account'] == 'No']
+minority_upsampled = resample(minority,
+                              replace=True,       # Sample with replacement
+                              n_samples=len(majority),  # Match majority class count
+                              random_state=42)   # For reproducibility
+# Combine majority class with upsampled minority class
+balanced_df = pd.concat([majority, minority_upsampled])
+
+# Shuffle the new balanced dataset
+balanced_df = balanced_df.sample(frac=1, random_state=42).reset_index(drop=True)
 # Split the data into features(X) and target(y)
-X = financial_encode.drop(['bank_account'], axis =1)
-y = financial_encode['bank_account']
+X = balanced_df.drop(['bank_account'], axis =1)
+y = balanced_df['bank_account']
 # Split the data into train and test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 @ st.cache_resource
@@ -64,32 +91,17 @@ def financial_train(X, y):
     scaler = StandardScaler()
     # Fit the X_train set into StandardScaler to scale it
     X_train_scaled = scaler.fit_transform(X_train)
-    # Instantiate RandomForestClassifier and RandomUnderSampler
-    rus = RandomUnderSampler()
+    # Instantiate RandomForestClassifier
     clf = RandomForestClassifier()
-    # Fit the RandomUnderSampler
-    X_resampled, y_resampled = rus.fit_resample(X_train_scaled, y_train)
     # Fit the data into randomforest
-    clf.fit(X_resampled, y_resampled)
+    clf.fit(X_train_scaled, y_train)
     return clf
 data_train = financial_train(X_train, y_train)
-# Predict the data
-results = X_test.copy()
-predict = data_train.predict(X_test)
-results['predicted_churn'] = predict
 # Create the prediction widget on the app
 st.subheader('Predictions')
-st.write(results.head())
-# Prepare a DataFrame for the user inputs features
-user_input = pd.DataFrame({'Country': [country],
-                          'Location': [location],
-                          'Cellphone': [cellphone],
-                          'Household_Size': [household_size],
-                          'Respondent_Age': [respondent_age],
-                          'Gender': [gender],
-                          'Relationship': [relationship]})
+st.write(inputs_df)
 # Encode user_input to match training data
-user_input_encoded = pd.get_dummies(user_input, columns = user_input.select_dtypes(include = 'object').columns)
+user_input_encoded = pd.get_dummies(inputs_df, columns = inputs_df.select_dtypes(include = 'object').columns)
 user_input_encoded = user_input_encoded.reindex(columns = X.columns, fill_value = 0)
 # Predict user_input
 user_prediction = data_train.predict(user_input_encoded)[0]
